24578c357a6ad7d48b24be071df07d5e38be725c
RuslanMirasov <info@mirasov.dev>
2023-10-27 21:01:03 +0200

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,18 +2,6 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-// class Component {
-//   constructor (public props:T) {
-
-//   }
-// }
-
-// class Page extends Component {
-//   pageInfo () {
-//     console.log(this.props.title);
-//   }
-// }
-
 interface ComponentProps {
   title: string;
 }
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -7,19 +7,6 @@
   Виправте тип у аргументі функції так, щоб не було помилок типу.
 */
 
-// type User = {
-//   name: string;
-//   surname: string;
-//   email: string;
-//   password: string;
-// }
-
-// function createOrUpdateUser(initialValues: User) {
-//   // Оновлення користувача
-// }
-
-// createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
-
 type User = {
   name: string;
   surname: string;
